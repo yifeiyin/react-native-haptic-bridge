2cdbd5290db8cbd778995082e720c5bf6ab90a75
Yifei Yin <yifeiyin@foxmail.com>
2021-05-25 14:00:00 -0400

    All works

---
+++ ios/RCTCalendarModule.h
@@ -0,0 +1,12 @@
+//
+//  RCTCalendarModule.h
+//  myapp
+//
+//  Created by Yifei Yin on 2021-05-25.
+//
+
+#ifndef RCTCalendarModule_h
+#define RCTCalendarModule_h
+
+
+#endif /* RCTCalendarModule_h */
---
+++ ios/RCTCalendarModule.m
@@ -0,0 +1,8 @@
+//
+//  RCTCalendarModule.m
+//  myapp
+//
+//  Created by Yifei Yin on 2021-05-25.
+//
+
+#import <Foundation/Foundation.h>
