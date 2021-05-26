db1535ae12ea9f9b1cfcf2c1017253ab37dc751a
Yifei Yin <yifeiyin@foxmail.com>
2021-05-26 00:08:47 -0400

    Add README

---
+++ README.md
@@ -0,0 +1,3 @@
+- [iOS Native Modules · React Native](https://reactnative.dev/docs/native-modules-ios#exporting-swift)
+- [Core Haptics | Apple Developer Documentation](https://developer.apple.com/documentation/corehaptics?language=objc)
+- [CHHapticEngine | Apple Developer Documentation](https://developer.apple.com/documentation/corehaptics/chhapticengine?language=objc)
--- ios/RCTCalendarModule.m
+++ ios/RCTCalendarModule.m
@@ -7,6 +7,7 @@
 
 #import <Foundation/Foundation.h>
 #import <React/RCTLog.h>
+//#import <AVKit/AVKit.h>
 #import <CoreHaptics/CoreHaptics.h>
 
 #import "RCTCalendarModule.h"
@@ -19,13 +20,17 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(createCalendarEvent:(NSString *)patternStr location:(NSString *)location)
 {
-//  NSString * patternStr = @"{  \"Version\": 1.0,  \"Metadata\":        {            \"Project\" : \"Haptic Sampler\",            \"Created\" : \"5 June 2019\",            \"Description\" : \"A sequence of tightly spaced transient events and parameter variations to create a gravel-like texture.\"        },  \"Pattern\":   [    {      \"ParameterCurve\": {        \"ParameterID\": \"HapticIntensityControl\",        \"Time\": 0.0,        \"ParameterCurveControlPoints\":         [          {            \"Time\": 0,            \"ParameterValue\": 0.3          },          {            \"Time\": 1.0,            \"ParameterValue\": 0.6          },          {            \"Time\": 3.0,            \"ParameterValue\": 0.0          }        ]      }    },    {      \"Event\": {        \"Time\": 0.04,        \"EventType\": \"HapticTransient\",        \"EventParameters\":         [          {            \"ParameterID\": \"HapticIntensity\",            \"ParameterValue\": 0.7          },          {            \"ParameterID\": \"HapticSharpness\",            \"ParameterValue\": 0.3          }        ]      }    },    {      \"Event\": {        \"Time\": 0.05,        \"EventType\": \"HapticTransient\",        \"EventParameters\":         [          {            \"ParameterID\": \"HapticIntensity\",            \"ParameterValue\": 1.0          },          {            \"ParameterID\": \"HapticSharpness\",            \"ParameterValue\": 0.5          }        ]      }    }]}";
   self->patternData = [patternStr dataUsingEncoding:NSUTF8StringEncoding];
   
   // Create an error variable through which the engine returns error information.
   NSError* error = nil;
 
+//  let audioSession = AVAudioSession.sharedInstance()
+//  engine = try CHHapticEngine(audioSession: [AVAudioSession sharedInstance])
+
+  
   // (1.) Create an instance of a haptic engine.
+//  self->hapticEngine = [[CHHapticEngine alloc] initWithAudioSession:[AVAudioSession sharedInstance] error:&error];
   self->hapticEngine = [[CHHapticEngine alloc] initAndReturnError:&error];
 
   // (2.) Start the haptic engine.
